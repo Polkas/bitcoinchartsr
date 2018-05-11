1afd5645b9509fcc11d4ecc0b82226144d7d2458
Maciej Nasinski <nasinski.maciej@gmail.com>
2018-05-11 22:58:58 +0200

    Tutor

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Using high frequency bitcoin quotes from the bitcoincharts website.
 This project provide a bunch of functions which enabled to download and preprocess this data.  
 Moreover there was built an example concerning usage of mentioned data.  
 More precisely an investigation of a price mean spillover across Bitcoin markets was performed - a test for the market efficiency. 
+
+bitcoincharts data usage - **upto a few hundred millions rows for all currencies and around 100 millions for USD markets**:
+
+`R_funs.R` - Tutorial of downloading and preprocessing this data   
+`bitcoin_spillovers.Rmd` - Exploration of Bitcoin markets efficiency
+
+![Bitcoin prices](./png/Prices_USD.png)
+
+![Bitcoin returns](./png/Returns_USD.png)
--- R_funs.R
+++ R_funs.R
@@ -1,19 +1,17 @@
 
-
-pacman::p_load(rvest,
-               tidyverse,
+pacman::p_load(dplyr,
+               magrittr,
+               rvest,
+               doParallel,
                stringr,
                lubridate,
                data.table,
-               doParallel,
-               parallel,
+               ggplot2,
                scales,
-               Hmisc,
-               MSBVAR,
                ggthemes,
-               rjags)
-
-
+               reshape2
+               )
+  
 #Database was built from data offered by [**bitcoincharts**](https://bitcoincharts.com). First of all there was downloaded files in [**csv.gz**](http://api.bitcoincharts.com/v1/csv/) format which contain quotes from active USD Bitcoin markets. Procedure assume that csv files are frequently update so if files are change re-run of script will cause downloading of files with a new upload time-stamp. The certain csv file contains all transactions from a particular bitcoin market at specific time frame. There are 3 attributes describing each file - time-stamp,price and volume of every transaction. This files could be merged and/or aggregate by time-stamp. Aggregating could be helpful because of the big size of decompressed files but at expense of lossing information. Data could be aggregate by every minute,hour or day. Data aggregation with 1 hour interval was implemented. Then statistics like mean,max.min,first and/or last price was evaluated.
 
 
@@ -34,106 +32,73 @@ csv_markets = markets_raw %>%
   html_nodes("a") %>%
   html_text() 
 
-csv_dates_m = markets_raw %>%
+csv_dates = markets_raw %>%
   html_nodes(xpath="/html/body/pre/text()") %>%
   html_text() %>%
   str_extract("\\d\\d-[a-zA-z].+-\\d\\d\\d\\d \\d\\d:\\d\\d")
 
-data.frame(csv_dates_m,csv_markets)
+tibble(csv_dates,csv_markets)
   
 }
 
 
 ###Active Markets List
 
-active_markets = function(curr=c('ARS','AUD','BRL','CAD','CHF','CLP','CZK','DKK','EUR','GBP','HKD','IDR',
-                                 'ILS','INR','JPY','KRW','MXN','MYR','NGN','NOK','NZD','PKR','PLN','RUB',
-                                 'SEK','SGD','SLL','THB','USD','VEF','VND','ZAR')){
-  if(!tryCatch(get("active_BCC_logi"),error = function(e) FALSE)){
+active_markets = function(){
+  
   a_markets=read_html("https://bitcoincharts.com/markets/list/") %>%
     html_nodes("body > div.container_16.content.padding > div > div.grid_7.alpha > ul") %>% html_text() %>% str_replace_all("\\n","") %>% str_replace_all("\\s+"," ") %>% str_trim()
   a_currency = read_html("https://bitcoincharts.com/markets/list/") %>%
     html_nodes("body > div.container_16.content.padding > div > div.grid_7.alpha > h3") %>% html_text()
   
-  DT = data.table(data.frame(a_currency,a_markets))
-  setkey(DT,"a_currency")
-  DT[curr]
-  assign("active_BCC",DT,envir = .GlobalEnv)
-  assign("active_BCC_logi",TRUE,envir = .GlobalEnv)
-  }
-  get("active_BCC")
+  clean_nams = function(x) gsub(")$","",trimws(unlist(str_split(x,"\\(|\\)\\s"))))
+  by_pos = function(x,by,pos) x[seq(pos,length(x),by)]
+  
+  DF = tibble(a_currency,a_markets) %>% rowwise()  %>% mutate(nam1=list(by_pos(clean_nams(a_markets),2,1)),nam2=list(by_pos(clean_nams(a_markets),2,2)))
+  
+  DF
+  
 }
 
-matrix(unlist(strsplit(as.character(active_markets()$a_markets)," \\(|\\) ")),ncol=2,byrow=T) %>% apply(2,function(x) str_replace_all(x,"\\)",""))
+active_m = active_markets() %>% filter(grepl("USD",a_currency))
 
+csv_files = csvs()
+sub = gsub(".csv.gz","",csv_files$csv_markets) %in% unlist(active_m$nam1)
+markets_sub = csv_files$csv_markets[sub]
 
-sub = gsub(".csv.gz","",csv_markets) %in% active_m
-markets_sub = markets[sub]
-dates_m_sub = as.Date(strftime(dmy_hm(csv_dates_m[sub]),"%Y-%m-%d"))
-
-###Downloading and Delete old files after update
-files_ls = list.files(paste0("~/bitcoinDB/"))
-for(i in seq_along(markets_sub)){
-  pos = str_detect(files_ls,markets_sub[i])
-  file_name_old = files_ls[pos]
-  file_name_new = paste0(dates_m_sub[i],"_",markets_sub[i])
-  if( (!any(pos))){
-    download.file(paste0("http://api.bitcoincharts.com/v1/csv/",markets_sub[i]),
-                  paste0("~/bitcoinDB/",file_name_new))
-  } else if( any(pos) & as.Date(strsplit(files_ls[pos],"_")[[1]][1]) < as.Date(dates_m_sub[i])){
-    system(paste0("bash | rm /mnt/c/Users/user/Documents/bitcoinDB/",file_name_old))
-    download.file(paste0("http://api.bitcoincharts.com/v1/csv/",markets_sub[i]),
-                  paste0("~/bitcoinDB/",file_name_new))
-  } 
-} 
-
-for(i in seq_along(markets_sub)){
-  pos = str_detect(markets_sub,strsplit(files_ls[i],"_")[[1]][2])
-  if(!any(pos)) system(paste0("bash | rm /mnt/c/Users/user/Documents/bitcoinDB/",files_ls[i]))
-}
-
-
-###File size
-
-s = file.info(paste0("C:/Users/user/Documents/bitcoinDB/",list.files("C:/Users/user/Documents/bitcoinDB/")))
-s_size = s$size
-names(s_size) = basename(rownames(s))
-
-markets = list.files("C:/Users/user/Documents/bitcoinDB/")
-
-markets = markets[!markets %in% basename(rownames(s))[s$size<1000]]
-
-markets_nam = markets %>% 
-  str_replace_all("USD.csv.gz","") %>%
-  strsplit("_")
-
-markets_nam = do.call(rbind,markets_nam)[,2]
-
-###Binding
+###Download and Binding Data - around 100 millions rows
+### It takes a few minutes
 
 cl <- makePSOCKcluster(max(parallel::detectCores()-1,1))
 doParallel::registerDoParallel(cl)
-bitcoin_merged = foreach(i = 1:length(markets),.combine = "rbind") %dopar%
+bitcoin_merged = foreach(i = 1:length(markets_sub),.combine = "rbind") %dopar%
 {
-  my_data = readr::read_csv(paste0("C:/Users/user/Documents/bitcoinDB/",markets[i]),col_names = c("time","price","vol"),col_types =  "idd") 
-  my_data$name = markets_nam[i]
+  my_data = tryCatch(readr::read_csv(paste0("http://api.bitcoincharts.com/v1/csv/",markets_sub[i]),col_names = c("time","price","vol"),col_types =  "idd"),
+                     error= function(e) data.frame(time=NA,price=NA,vol=NA))
+  
+  my_data$name = gsub(".csv.gz","",markets_sub[i])
   my_data$time = as.POSIXct(my_data$time,origin = "1970-01-01")
   my_data
 }
 stopCluster(cl)
 
+saveRDS(bitcoin_merged,paste0("bitcoinUSD_",Sys.Date(),".Rds"))
 
-###Aggregating
-bitcoin_agg = data.table(bitcoin_merged)
-bitcoin_agg  = bitcoin_agg[,.(
+#bitcoin_merged = readRDS("bitcoinUSD_2018-05-12.Rds")
+
+###Aggregating - to x interval
+setDT(bitcoin_merged)
+
+bitcoin_agg  = bitcoin_merged[,c('time_YmdH') := list(lubridate::floor_date(time,"hour"))] %>%
+  .[,.(
   #price_max=max(price,na.rm=T),
   #price_min = min(price,na.rm=T),
-  #price_mean = mean(price,na.rm=T),
+  price_mean = mean(price,na.rm=T),
   price_first = first(price),
   price_last = last(price),
   vol_sum = sum(vol,na.rm=T)),
   #vol_max = max(vol,na.rm=T)),
-  by=list(time_YmdH =  floor_date(time,"minute"),name)]
+  by=list(name,time_YmdH)]
 
 bitcoin_agg[,ret_fl := (log(price_last)-log(price_first)),by=list(name)]
 
@@ -141,45 +106,33 @@ rm(bitcoin_merged)
 
 bitcoin_spread = dcast(bitcoin_agg[,.(time_YmdH,name,ret_fl)],time_YmdH ~ name,value.var="ret_fl")
 
-
-###Deleting markets with hidg NA ratio
+###Deleting markets with high NA ratio
 
 low_NA = colnames(bitcoin_spread)[sort(apply(bitcoin_spread,2,function(x) sum(is.na(x))/length(x)),index.return=T)$ix[1:7]]
 
-bitcoin_spread = na.omit(bitcoin_spread[,low_NA,with=F])
+#bitcoin_spread = na.omit(bitcoin_spread[,low_NA,with=F])
 
-dat = bitcoin_agg[time_YmdH>=min(bitcoin_spread$time_YmdH) & time_YmdH<max(bitcoin_spread$time_YmdH) & name %in% low_NA,,][order(time_YmdH)]
+frams = bitcoin_agg[name %in% low_NA,.(min_t=min(time_YmdH),max_t=max(time_YmdH)),by=.(name)][,.(min=max(min_t),max=min(max_t))]
 
-
-##In most cases it is much easier to analyse returns not prices because of stationarity of a former. Example of a stationary process is the white noise process which probability distribution does not changed during time. The returns are calculated as log difference between first and last transaction price at a specific hour.
-##List of six active and most volatile USD Bitcoin markets: `r toupper(low_NA[-1])`
-
+dat = bitcoin_agg[time_YmdH>=frams$min & time_YmdH<frams$max & name %in% low_NA,,][order(time_YmdH)]
 
 g1 = ggplot(dat,
-            aes(x=time_YmdH,y=price_last,color=name))  + 
+            aes(x=time_YmdH,y=price_mean,color=name))  + 
   ggthemes::theme_tufte()+
-  geom_line() + 
+  geom_line(size=2) + 
   scale_x_datetime(breaks = date_breaks("3 month"),labels = date_format("%Y-%m")) +
   theme(axis.text.x = element_text(angle = 90, vjust=0.5)) +
   facet_wrap(~name,scales = "free",ncol=2) + 
-  ggtitle("Bitcoin prices at active and most volatile Bitcoin markets")
+  ggtitle("Bitcoin prices at active and most volatile Bitcoin USD markets - by hour")
 
 g2 = ggplot(dat,
             aes(x=time_YmdH,y=ret_fl,color=name))  + 
   ggthemes::theme_tufte()+
-  geom_line() + 
+  geom_line(size=2) + 
   scale_x_datetime(breaks = date_breaks("3 month"),labels = date_format("%Y-%m")) +
   theme(axis.text.x = element_text(angle = 90, vjust=0.5)) +
   facet_wrap(~name,scales = "free",ncol=2) + 
-  labs(title = "Bitcoin returns at active and most volatile Bitcoin markets - by hour") 
+  labs(title = "Bitcoin returns at active and most volatile Bitcoin USD markets - by hour") 
 
-ggplot(dat,
-       aes(x=time_YmdH,y=ret_fl,color=name))  + 
-  ggthemes::theme_tufte()+
-  geom_line() + 
-  scale_x_datetime(breaks = date_breaks("3 month"),labels = date_format("%Y-%m")) +
-  theme(axis.text.x = element_text(angle = 90, vjust=0.5)) +
-  facet_wrap(~name,scales = "free",ncol=2) + 
-  labs(title = "Bitcoin returns at active and most volatile Bitcoin markets - by hour") 
-
-g2
+ggsave(g1,"Prices_USD.png")
+ggsave(g2,"Returns_USD.png")
